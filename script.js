dc34a48d9a3c599813b669cedc95a501bda0b007
Oleg Tarasenko <gryzovick2@gmail.com>
2023-06-04 20:05:34 +0300

    JS Video 1-3

---
+++ script.js
@@ -0,0 +1,60 @@
+// Data types 
+
+// String
+const greetings = "Hello world";
+
+console.log('Primitives')
+console.log(typeof greetings);
+
+// Number
+const num = 100;
+console.log(typeof num);
+
+// Boolean
+const boolTrue = true;
+const boolFalse = false;
+console.log(typeof boolFalse);
+
+// Null
+
+const someNullValue = null;
+console.log(someNullValue);
+let userImg = null;
+
+// undefined
+
+const someUndefinedValue = undefined;
+console.log(someUndefinedValue);
+
+
+
+// object
+
+const obj = {}
+    obj.name = "Vitality";
+    obj.hobby = "football";
+    obj["age"] = 26;
+
+console.log(typeof obj);
+
+// Big Int
+const bigInt = 99999999999999999999n;
+console.log(typeof bigInt);
+console.log(bigInt);
+
+// Symbol
+
+const user = {
+    id: 10,
+    age: 20,
+    height: 170,
+}
+
+const id = Symbol('some new symbol');
+user[id] = 500;
+console.log(typeof id);
+
+
+
+console.log(typeof obj);
+
